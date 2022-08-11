e6382b7d33b47ad9f316036f93e7efe6d137412f
Krystian Szlagor <szlagor.k@onet.pl>
2022-08-11 10:10:22 +0200

    Create TrackManager script

---
+++ DarkTunnels/Assets/Scripts/Train/TrackManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+namespace DarkTunnels
+{
+    public class TrackManager : MonoBehaviour
+    {
+        public static TrackManager CurrentTrackManager { get; set; }
+
+        [field: Space, Header("Path")]
+        [field: SerializeField]
+        private CinemachineSmoothPath Path { get; set; }
+        [field: SerializeField]
+        private GameObject PathPrefab { get; set; }
+
+        [field: Space, Header("Add track input")]
+        [field: SerializeField]
+        private KeyCode AddTrackInput { get; set; }
+
+        [field: Space, Header("Generator setttings")]
+        [field: SerializeField]
+        private int Distance { get; set; }
+
+        private CinemachineSmoothPath.Waypoint[] WaypointsCollection { get; set; }
+        private int CurrentWaypointIndex { get; set; }
+        private Vector3 PathSpawnPosition { get; set; }
+
+        protected virtual void Awake ()
+        {
+            SetSingleton();
+            Initialize();
+        }
+
+        private void SetSingleton ()
+        {
+            if (CurrentTrackManager != null && CurrentTrackManager != this)
+            {
+                Destroy(gameObject);
+            }
+
+            CurrentTrackManager = this;
+        }
+
+        private void Initialize ()
+        {
+            GenerateTrack();
+
+            CurrentWaypointIndex = 0;
+            WaypointsCollection = new CinemachineSmoothPath.Waypoint[transform.childCount + 1];
+
+            for (int i = 0; i< transform.childCount; i++)
+            {
+                Transform currentChild = transform.GetChild(i);
+
+                if (i == 0)
+                {
+                    AddWaypoint(currentChild, 0);
+                }
+
+                AddWaypoint(currentChild, 1);
+            }
+
+            Path.m_Waypoints = WaypointsCollection;
+        }
+
+        private void GenerateTrack ()
+        {
+            for (int i=0; i<Distance; i++)
+            {
+                AddTrack();
+            }
+        }
+
+        private void AddTrack ()
+        {
+            Instantiate(PathPrefab, PathSpawnPosition, Quaternion.identity, transform);
+            PathSpawnPosition += new Vector3(0, 0, 25);
+        }
+
+        private void AddWaypoint(Transform child, int index)
+        {
+            CinemachineSmoothPath path = child.GetComponent<CinemachineSmoothPath>();
+
+            CinemachineSmoothPath.Waypoint wp = path.m_Waypoints[index];
+            CinemachineSmoothPath.Waypoint targetWp = new CinemachineSmoothPath.Waypoint();
+
+            targetWp.position = child.rotation * wp.position + child.localPosition;
+            targetWp.roll = wp.roll;
+
+            WaypointsCollection[CurrentWaypointIndex] = targetWp;
+            CurrentWaypointIndex++;
+        }
+    }
+}
