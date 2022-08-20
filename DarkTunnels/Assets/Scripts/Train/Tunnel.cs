6d6c002467a3d5883a6075900f69f162f10e1902
Krystian Szlagor <szlagor.k@onet.pl>
2022-08-20 11:41:00 +0200

    Create Tunnel script

---
+++ DarkTunnels/Assets/Scripts/Train/Tunnel.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Cinemachine;
+
+public class Tunnel : MonoBehaviour
+{
+    [field: Space, Header("Path refrence")]
+    [field: SerializeField]
+    private CinemachinePath Path { get; set; }
+
+    [field: Space, Header("EndTunnel refrence")]
+    [field: SerializeField]
+    private Transform EndTunnel { get; set; }
+}
